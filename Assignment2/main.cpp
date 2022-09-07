1a9477c34588c058a06aa172eb1f3164b2d9a28e
mateajinkya <93010008+mateajinkya@users.noreply.github.com>
2022-09-07 14:02:08 -0700

    "Committing changes locally. "

---
+++ Assignment2/main.cpp
@@ -0,0 +1,10 @@
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <scanner.h>
+
+using namespace std;
+
+// Main program where the nexttoken() function will be called. 
