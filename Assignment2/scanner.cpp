4f843700770f63167c3099207eba96547ef8f4ec
smei <smei0528@gmail.com>
2022-09-07 02:10:50 -0700

    Added first state of FSM
    +Added state to check if identifier or keyword

--- Assignment2/scanner.cpp
+++ Assignment2/scanner.cpp
@@ -5,37 +5,34 @@ using namespace std;
 
 bool Scanner::isSpecialSymbol(char ch) {
     // Check if there is a special character in the input stream. 
-    if (ch == ' ' || ch == '+' || ch == '-' || ch == '*' ||
+    if (ch == '+' || ch == '-' || ch == '*' ||
         ch == '/' || ch == ',' || ch == '.' || ch == ';' ||
         ch == ':=' || ch == '(' || ch == ')' || ch == '=' ||
         ch == '<' || ch == '>' || ch == '{' || ch == '}' ||
-        ch == '[' || ch == ']')
+        ch == '[' || ch == ']' || ch == '\'')
+    {
+        return true;
+    }
+    return false;
+}
+bool Scanner::isAlpha(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        return true;
+    }
+    return false;
+}
+bool Scanner::isDigit(char ch) {
+    // Not an identifier if the first character is an int or a special symbol.
+    if (ch == '0' || ch == '1' || ch == '2' ||
+        ch == '3' || ch == '4' || ch == '5' ||
+        ch == '6' || ch == '7' || ch == '8' ||
+        ch == '9')
     {
         return true;
     }
     return false;
 }
 
-bool Scanner::isIdentifier(char* str) {
-    // Not an identifier if the first character is an int or a special symbol.
-    if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
-        str[0] == '3' || str[0] == '4' || str[0] == '5' ||
-        str[0] == '6' || str[0] == '7' || str[0] == '8' ||
-        str[0] == '9' || isSpecialSymbol(str[0]) == true)
-    {
-        return false;
-    }
-    // If string length is one, it's already an identifier. 
-    if (strlen(str) == 1) return true;
-    // Otherwise, check for special symbols. 
-    else {
-        for (int i = 1; i < strlen(str); i++) {
-            if (isSpecialSymbol(str[i] == true)) return false;
-        }
-    }
-    // Return true by default. 
-    return true;
-}
 
 bool Scanner::isNumber(char* str) {
     int numOfDecimal = 0;
@@ -108,10 +105,8 @@ string Scanner::searchTable(string word) {    //implemented with Binary Search O
         }
     }
 }
-char Scanner::nexttoken(string line, int i) {
-    while (line[i] != ' ' || line[i] != '\n' || line[i] != '\t') {
-
-    }
-    char token;
-    return token;
+char Scanner::nexttoken(istream& in) {
+    char next_ch;
+    in >> next_ch;
+    return next_ch;
 }
--- Assignment2/scanner.h
+++ Assignment2/scanner.h
@@ -7,8 +7,9 @@ using namespace std;
 
 class Scanner {
 public:
-    char nexttoken(string line, int i);
-    bool isIdentifier(char*);
+    char nexttoken(istream& in);
+    bool isAlpha(char ch);
+    bool isDigit(char ch);
     bool isSpecialSymbol(char);
     bool isNumber(char*);
     bool isReservedWord(char*);
@@ -17,14 +18,13 @@ public:
 
 private:
     vector<string> table = { "AND", "ARRAY", "ASM", "BEGIN", "BREAK", "CASE",
-     "CONST", "CONSTRUCTOR", "CONTINUE", "DESTRUCTOR", "DIV", "DO", "DOWNTO", 
-    "ELSE", "END", "FALSE", "FILE", "FOR", "FUNCTION", "GOTO", "IF", "IMPLEMENTATION", 
-    "IN", "INLINE", "INTERFACE", "LABEL", "MOD", "NIL", "NOT", "OBJECT", "OF", "ON", "OPERATOR", 
-    "OR", "PACKED", "PROCEDURE", "PROGRAM", "RECORD", "REPEAT", "SET", "SHL", "SHR", 
-    "STRING", "THEN", "TO", "TRUE", "TYPE", "UNIT", "UNTIL", "USES", "VAR", "WHILE", "WITH", 
-    "XOR", "INTEGER", "REAL", "IDENTIFIER", "PLUSOP", "MINUSOP", "MULTOP", "DIVOP", "ASSIGN", 
-    "EQUAL", "NE", "LTEQ", "GTEQ", "LT", "GT", "PLUSEQUAL", "MINUSEQUAL", "MULTEQUAL", 
-    "DIVEQUAL", "CARAT"}; //Table has IDENTIFIER spelled as INDENTIFIER?? mistake i dunno lmao
+     "CONST", "CONSTRUCTOR", "CONTINUE", "DESTRUCTOR", "DIV", "DO", "DOWNTO",
+    "ELSE", "END", "FALSE", "FILE", "FOR", "FUNCTION", "GOTO", "IF", "IMPLEMENTATION",
+    "IN", "INLINE", "INTERFACE", "LABEL", "MOD", "NIL", "NOT", "OBJECT", "OF", "ON", "OPERATOR",
+    "OR", "PACKED", "PROCEDURE", "PROGRAM", "RECORD", "REPEAT", "SET", "SHL", "SHR",
+    "STRING", "THEN", "TO", "TRUE", "TYPE", "UNIT", "UNTIL", "USES", "VAR", "WHILE", "WITH",
+    "XOR", "INTEGER", "REAL", "IDENTIFIER", "PLUSOP", "MINUSOP", "MULTOP", "DIVOP", "ASSIGN",
+    "EQUAL", "NE", "LTEQ", "GTEQ", "LT", "GT", "PLUSEQUAL", "MINUSEQUAL", "MULTEQUAL",
+    "DIVEQUAL", "CARAT" }; //Table has IDENTIFIER spelled as INDENTIFIER?? mistake i dunno lmao
 };
 #endif
-
