f60601ea73cd23fffe3cdaad2528e500f55f471f
Sean Mei <42566561+mei-sean@users.noreply.github.com>
2022-09-07 16:08:02 -0700

    FSM 2/3
    +Added operator detection
    +Added Keyword and Identifier detection
    +changed search table to linear search

--- Assignment2/source.cpp
+++ Assignment2/source.cpp
@@ -12,9 +12,9 @@ int main()
     Scanner scan;
     ifstream ifs;
     string word;
-    int line = 0;
+    int line = 1;
     char ch;
-    bool error = false;
+    bool error;
 
     ifs.open("masterTestCase.txt");    //input file stream
     if (ifs.fail()) {
@@ -23,6 +23,7 @@ int main()
     }
     while (!ifs.eof()) {    //continue input file stream if not at end of file
         word = "";    //string to store current word
+        error = false;     //beginning of scan no error
         ch = scan.nexttoken(ifs);    //scan the first character in file 
         if (scan.isAlpha(ch)) {    //check if char is a letter
             //This block indicates word must be identifier or keyword
@@ -42,20 +43,44 @@ int main()
                 cout << "TOKEN ERROR at line " << line << endl;
             }
             else {
-
+                string token = (str_upper(word));
+                if (scan.searchTable(token) == token) {
+                    cout << token << " : " << word << endl;
+                }
+                else {
+                    cout << "Identifier : " << word << endl;
+                }
             }
             if (ch == '\n') {
                 line++;
             }
-            //table lookup on word 
-            //if returns print keyword
-            //otherwise print that it is a identifier
         }
         else if (scan.isDigit(ch)) {    //check if first char is a digit
             //This block indicates word must be a integer or real
+
         }
         else if (scan.isSpecialSymbol(ch)) {
             //This block indicates word must be a operator 
+            word.push_back(ch);    //append letter to current word
+            while (ch != '\n' && ch != '\t' && ch != ' ') {
+                if (scan.isDoubleSymbol(ch)) {
+                    ch = scan.nexttoken(ifs);
+                    string tempWord = word;
+                    tempWord.push_back(ch);
+                    string token = scan.searchTable(tempWord);
+                    if (token != "") {
+                        cout << token << " : " << tempWord << endl;
+                    }
+                }
+                else {
+                    string token = scan.searchTable(word);
+                    cout << token << " : " << word << endl;
+                }
+                ch = scan.nexttoken(ifs);
+                if (ch == '\n') {
+                    line++;
+                }
+            }
         }
         //check table 
     }
@@ -66,4 +91,5 @@ string str_upper(string word) {
     for (int i = 0; i < new_word.size(); i++) {
         new_word.at(i) = toupper(new_word.at(i));
     }
+    return new_word;
 }
